a57f76c301ce2013755884ff665bc688a0ee66d4
Patt126 <145294744+Patt126@users.noreply.github.com>
2026-02-11 18:14:29 +0100

    final

--- Matrix_elements_/Si copy/DFT/phonons/plot.py
+++
@@ -1,8 +0,0 @@
-import py4vasp
-
-calc = py4vasp.Calculation.from_path(".")
-ph = calc.phonon_band  # <-- questo è l'oggetto descritto nella doc
-
-# salva direttamente un'immagine (pdf/png ecc.)
-ph.to_image(filename="LiF_phonons_py4vasp.pdf")
-ph.to_image(filename="LiF_phonons_py4vasp.png")
